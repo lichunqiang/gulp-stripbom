f25b3da2aa36d3be696ef071e03238e01117e653
light <light-li@hotmail.com>
2014-07-11 15:27:59 +0800

    add jshint test

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var stripBom = require('./');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 //test buffer
 gulp.task('default', function(){
@@ -16,4 +18,10 @@ gulp.task('test-stream', function(){
 	return gulp.src('1.txt', {buffer: false})
 			.pipe(stripBom())
 			.pipe(gulp.dest('dest-stream'));
+});
+
+gulp.task('hint', function(){
+	return gulp.src('./index.js')
+			.pipe(jshint())
+			.pipe(jshint.reporter(stylish));
 });
--- index.js
+++ index.js
@@ -10,22 +10,26 @@ var symbols = {
     ok: '√',
     err: '×'
 };
-if ('win32' == process.platform) {
+if ('win32' === process.platform) {
     symbols.ok = '\u221A';
     symbols.err = '\u00D7';
 }
 
 module.exports = function(opts){
-    if(!opts) opts = {};
-    opts.showLog === undefined && (opts.showLog = true);
+    if(!opts) {
+        opts = {};
+    }
+    if(opts.showLog === undefined) {
+        opts.showLog = true;
+    }
 
 	return through.obj(function(file, enc, cb){
-
 		//let null files pass through
         if (file.isNull()) {
             this.push(file);
             return cb();
         }
+
         var fileExt = path.extname(file.path).slice(1);
         //check file ext
         if(opts.ext) {
@@ -40,15 +44,16 @@ module.exports = function(opts){
 
         	file.contents = file.contents.pipe(stripBom.stream());
         } 
+
         //is buffer
         if(file.isBuffer()) {
 
         	file.contents = stripBom(file.contents);
         }
+        
         // make sure the file goes through the next gulp plugin
         this.push(file);
         if(opts.showLog) {
-            
             gutil.log(gutil.colors.cyan(PLUGIN_NAME + ':'), file.relative + ' ' + gutil.colors.green(symbols.ok));
         }
 
@@ -57,4 +62,4 @@ module.exports = function(opts){
 	}).on('error', function(err){
 		throw new gutil.PluginError(PLUGIN_NAME, err.message);
 	});
-}
+};
